[FIX] calculation of completion rate

maddpg/metrics.py
@@ -18,13 +18,13 @@ def completion_rate(eval_env):
     world = eval_env.world
     landmarks = np.array([l.state.p_pos for l in world.landmarks])
     agents = np.array([a.state.p_pos for a in world.agents])
-    agent_diameter = world.agents[0].size * 2
+    agent_radius = world.agents[0].size
 
     cover_cnt = 0
     # simple (n^2) solution
     for landmark in landmarks:
         distances = np.sqrt(np.square(landmark - agents).sum(axis=1))
-        covered = distances.min() < agent_diameter
+        covered = distances.min() < agent_radius
         if covered:
             cover_cnt += 1
 